6c1fb7bb3480853016d1a67c29ccfd13f1315e7d
rasbihari Das <48406292+rdas77@users.noreply.github.com>
2023-11-26 20:18:23 +0530

    Add files via upload

--- streamlit_UI.py
+++ streamlit_UI.py
@@ -28,7 +28,7 @@ st.markdown("## " + 'All Symbol/Any Symbol/Top 5 Weight')
 st.markdown("#### " +"What Trends would you like to see?")
 
 selected_metrics = st.selectbox(
-    label="Choose...", options=['AllSymbol','AnySymbol','Top5','Incranation','Incranation History']
+    label="Choose...", options=['AllSymbol','AnySymbol','Top5','Incranation','Incranation History','History']
 )
 
 if selected_metrics=='Top5':
@@ -106,15 +106,8 @@ if selected_metrics=='AnySymbol':
         st.write(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{row}</p>', unsafe_allow_html=True)
          
 if selected_metrics=='Incranation History':
-    
-
-    
-
-
-
 #Converting string date into datetime object
-
-
+       
     ip_date =st.date_input("Any trading date", format="DD/MM/YYYY", disabled=False, label_visibility="visible")
     #from datetime import datetime 
     #dateTimeObj = str(datetime.strptime(ip_date, "%d-%b-%Y") )
@@ -153,8 +146,47 @@ if selected_metrics=='Incranation':
         st.write(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{row}</p>', unsafe_allow_html=True)
          
 
-            
-            
-        
+if selected_metrics=='History':
+    
+#Converting string date into datetime object
+    #seq_n=st.text_input('Enter seq number 1')
+    #symbol = st.text_input('Enter symbol Name')
+    selected_metrics_1 = st.selectbox(
+    label="Choose...", options=['Firstbatch','Symbol']
+    )
+    
+    ip_date =st.date_input("Any trading date", format="DD/MM/YYYY", disabled=False, label_visibility="visible")
+    if selected_metrics_1=='Firstbatch':
+        #from datetime import datetime 
+    #dateTimeObj = str(datetime.strptime(ip_date, "%d-%b-%Y") )
+        dateTimeObj=str(ip_date)
+        st.write(dateTimeObj)
+    # Execute a sample query
+        #query = "SELECT 'CUL_WEIGHT_N~'||CUL_WEIGHT_N||'~OPEN_PRICE~'|| OPEN_PRICE ||'~MAX PRICE~'|| A.MAX_PRICE_N||'~MIN_PRICE~' || MIN_PRICE_N ||'~PREV_CLOSE~'||PREV_CLOSE ||'~LASTPRICE_N~'||LASTPRICE_N||'~BSPL~'||CASE WHEN LAST_AVG_PRICE_N > LASTPRICE_N THEN 'S->'||BUYER_SELLER_PER_LAKH_N  ELSE 'B->'||BUYER_SELLER_PER_LAKH_N  END|| CASE WHEN LAST_AVG_PRICE_N > LASTPRICE_N THEN 'S->'||BUYER_SELLER_PER_LAKH_N  ELSE  'B->'||BUYER_SELLER_PER_LAKH_N END||'~SYMBOL~'|| A.SYMBOL,'~TIME~'|| TO_CHAR(LAST_UPDATE_DT,'DD-MON-YY HH24:MI')||'~SEQ_N~'|| DAILY_BATCH_SEQ_N   FROM DAILY_NSE_MOVEMENT_TRANS_HIST A, FIVEDAYS_AVG_TBL_HIST B WHERE   BUYER_SELLER_PER_LAKH_N>0 AND BUYER_SELLER_PER_LAKH_N IS NOT NULL AND  A.SYMBOL=B.SYMBOL AND  to_char(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+" AND to_char(RECORD_DATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+"  AND DAILY_BATCH_SEQ_N=1 order by  DAILY_BATCH_SEQ_N"
+        query ="SELECT remark_v   FROM DAILY_SHORTSELL_CALL_HIST A WHERE TO_CHAR(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+"  order by  LAST_UPDATE_DT"
+    #st.write(query)
+        with connection.cursor() as cursor:
+             cursor.execute(query)
+             results = cursor.fetchall()
+    #Display the query results
+        st.write("Query Results:")
+        for row in results:
+            st.write(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{row}</p>', unsafe_allow_html=True)
+                    
+    if selected_metrics_1=='Symbol':        
+        symbol_1 = st.text_input('Enter symbol Name')
+        dateTimeObj=str(ip_date)
+        st.write(dateTimeObj)
+    # Execute a sample query
+        if symbol_1:
+            query = "SELECT 'SYMBOL~'|| A.SYMBOL,'~TIME~'|| TO_CHAR(LAST_UPDATE_DT,'DD-MON-YY HH24:MI')||'~LASTPRICE_N~'||LASTPRICE_N|| '~OPEN_PRICE~'|| OPEN_PRICE ||'~MAX PRICE~'|| A.MAX_PRICE_N||'~MIN_PRICE~' || MIN_PRICE_N ||'~PREV_CLOSE~'||PREV_CLOSE ||'~BSPL~'|| CASE WHEN LAST_AVG_PRICE_N > LASTPRICE_N THEN 'S->'||BUYER_SELLER_PER_LAKH_N  ELSE  'B->'||BUYER_SELLER_PER_LAKH_N END ||'CUL_WEIGHT_N~'||CUL_WEIGHT_N FROM DAILY_NSE_MOVEMENT_TRANS_HIST A, FIVEDAYS_AVG_TBL_HIST B WHERE   BUYER_SELLER_PER_LAKH_N>0 AND BUYER_SELLER_PER_LAKH_N IS NOT NULL AND  A.SYMBOL=B.SYMBOL AND  to_char(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+" AND to_char(RECORD_DATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+"  AND a.symbol='"+symbol_1+"'"+" order by  a.DAILY_BATCH_SEQ_N"
+            #st.write(query)
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                results = cursor.fetchall()
+            #Display the query results
+            st.write("Query Results:")
+            for row in results:
+                st.write(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{row}</p>', unsafe_allow_html=True)
         
         
