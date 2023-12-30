86d9ed438a65b449dbe049d5fa2af9dfd7284bd4
rasbihari Das <48406292+rdas77@users.noreply.github.com>
2023-12-30 19:06:09 +0530

    Update streamlit_UI.py
    New functionalities has been added 
    in historicalmodule

--- streamlit_UI.py
+++ streamlit_UI.py
@@ -28,38 +28,59 @@ st.markdown("## " + 'All Symbol/Any Symbol/Top 5 Weight')
 st.markdown("#### " +"What Trends would you like to see?")
 
 selected_metrics = st.selectbox(
-    label="Choose...", options=['AllSymbol','AnySymbol','Top5','Incranation','Incranation History','History']
+    label="Choose...", options=['AllSymbol','AnySymbol','Top5','Incranation','History']
 )
 
 if selected_metrics=='Top5':
+
+    def incranation(symbol):
+        query = "SELECT count(*) FROM DAILY_NSE_MOVEMENT_TRANS where  BUYER_SELLER_PER_LAKH_N > 3000 and symbol='"+symbol+"'"
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            results = cursor.fetchall()
+            for row in results:
+                return(row[0])
+            
+        
     
     
     symbol=[]
-    weight=[]    
+    weight=[]
+    incra=[]
     query = "SELECT * FROM (SELECT symbol ,max(cul_weight_n) FROM BATCH_BUY_SELL_SEQ_NEW_V group by symbol order by 2 desc) WHERE ROWNUM <6"
     with connection.cursor() as cursor:
+        
         cursor.execute(query)
         results = cursor.fetchall()
         
     for row in results:
+        
         symbol.append(row[0])
+        incra.append(incranation(row[0]))
         weight.append(row[1])
         
         
         
-    tuples = [(key, value) for i, (key, value) in enumerate(zip(symbol, weight))]
+    #tuples = [(key, value) for i, (key, value) in enumerate(zip(symbol, weight))]
     # convert list of tuples to dictionary using dict()
-    symbol_dic = dict(tuples)
+    #symbol_dic = dict(tuples)
     #print(symbol_dic)
-    symbol_1 = list(symbol_dic.keys())
-    weight_1 = list(symbol_dic.values())
+    #symbol_1 = list(symbol_dic.keys())
+    #weight_1 = list(symbol_dic.values())
     import matplotlib.pyplot as plt
     fig = plt.figure(figsize = (10, 5))
-    plt.barh(symbol_1 , weight_1)
-    plt.xlabel("Symbol")
-    plt.ylabel("Weight")
-    plt.title("Top 5 weighted symbol")
+    colors = ['r' if t > 1 else 'g' for t in incra]
+    plt.barh(symbol , weight,color=colors)
+    for index, value in enumerate(weight):
+            plt.text(value, index, str(value))
+    #from datetime import datetime
+    #plt.xlabel(str(datetime.strftime(ip_date, "%d-%b-%Y")))
+    #plt.title("Top 5 weighted symbol")
+    plt.title('Top 5 weighted symbol', fontweight='bold', color = 'red', fontsize='18')
     st.pyplot(fig)
+    
+
+
 
 
          
@@ -69,7 +90,7 @@ if selected_metrics=='AllSymbol':
     #st.write("Connected to Oracle database.")
 
     # Execute a sample query
-    query = "SELECT to_char(last_update_dt,'hh24:mi:ss'), remark_v FROM daily_shortsell_call order by last_update_dt"
+    query = "SELECT to_char(last_update_dt,'hh24:mi:ss'), remark_v FROM daily_shortsell_call where BUYER_SELLER_PER_LAKH_N > 2500 order by last_update_dt"
     with connection.cursor() as cursor:
         cursor.execute(query)
         results = cursor.fetchall()
@@ -105,28 +126,7 @@ if selected_metrics=='AnySymbol':
         #st.success(row)
         st.write(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{row}</p>', unsafe_allow_html=True)
          
-if selected_metrics=='Incranation History':
-#Converting string date into datetime object
-       
-    ip_date =st.date_input("Any trading date", format="DD/MM/YYYY", disabled=False, label_visibility="visible")
-    #from datetime import datetime 
-    #dateTimeObj = str(datetime.strptime(ip_date, "%d-%b-%Y") )
-    dateTimeObj=str(ip_date)
-    st.write(dateTimeObj)
-    # Execute a sample query
-    query = "SELECT symbol,cul_weight_n,RATIO_F_V FROM DAILY_NSE_MOVEMENT_TRANS_HIST where  BUYER_SELLER_PER_LAKH_N > 3000 and to_char(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+""
- 
-    #st.write(query)
-    with connection.cursor() as cursor:
-        cursor.execute(query)
-        results = cursor.fetchall()
 
-    # Display the query results
-    st.write("Query Results:")
-    for row in results:
-        #st.write(row)
-        #st.success(row)
-        st.write(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{row}</p>', unsafe_allow_html=True)
          
 if selected_metrics=='Incranation':
         
@@ -152,7 +152,7 @@ if selected_metrics=='History':
     #seq_n=st.text_input('Enter seq number 1')
     #symbol = st.text_input('Enter symbol Name')
     selected_metrics_1 = st.selectbox(
-    label="Choose...", options=['Firstbatch','Symbol']
+    label="Choose...", options=['Firstbatch','Symbol','Top5-History','Incranation History']
     )
     
     ip_date =st.date_input("Any trading date", format="DD/MM/YYYY", disabled=False, label_visibility="visible")
@@ -163,7 +163,7 @@ if selected_metrics=='History':
         st.write(dateTimeObj)
     # Execute a sample query
         #query = "SELECT 'CUL_WEIGHT_N~'||CUL_WEIGHT_N||'~OPEN_PRICE~'|| OPEN_PRICE ||'~MAX PRICE~'|| A.MAX_PRICE_N||'~MIN_PRICE~' || MIN_PRICE_N ||'~PREV_CLOSE~'||PREV_CLOSE ||'~LASTPRICE_N~'||LASTPRICE_N||'~BSPL~'||CASE WHEN LAST_AVG_PRICE_N > LASTPRICE_N THEN 'S->'||BUYER_SELLER_PER_LAKH_N  ELSE 'B->'||BUYER_SELLER_PER_LAKH_N  END|| CASE WHEN LAST_AVG_PRICE_N > LASTPRICE_N THEN 'S->'||BUYER_SELLER_PER_LAKH_N  ELSE  'B->'||BUYER_SELLER_PER_LAKH_N END||'~SYMBOL~'|| A.SYMBOL,'~TIME~'|| TO_CHAR(LAST_UPDATE_DT,'DD-MON-YY HH24:MI')||'~SEQ_N~'|| DAILY_BATCH_SEQ_N   FROM DAILY_NSE_MOVEMENT_TRANS_HIST A, FIVEDAYS_AVG_TBL_HIST B WHERE   BUYER_SELLER_PER_LAKH_N>0 AND BUYER_SELLER_PER_LAKH_N IS NOT NULL AND  A.SYMBOL=B.SYMBOL AND  to_char(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+" AND to_char(RECORD_DATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+"  AND DAILY_BATCH_SEQ_N=1 order by  DAILY_BATCH_SEQ_N"
-        query ="SELECT remark_v   FROM DAILY_SHORTSELL_CALL_HIST A WHERE TO_CHAR(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+"  order by  LAST_UPDATE_DT"
+        query ="SELECT  TO_CHAR(LAST_UPDATE_DT,'DD-MM-YY HH24:MI')||'~'||remark_v   FROM DAILY_SHORTSELL_CALL_HIST A WHERE TO_CHAR(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+"  order by  LAST_UPDATE_DT"
     #st.write(query)
         with connection.cursor() as cursor:
              cursor.execute(query)
@@ -188,5 +188,75 @@ if selected_metrics=='History':
             st.write("Query Results:")
             for row in results:
                 st.write(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{row}</p>', unsafe_allow_html=True)
+    if selected_metrics_1=='Top5-History':
         
-        
+        def incranation(symbol):
+            query = " SELECT * FROM (SELECT DISTINCT RATIO_F_V FROM INCR_VW_HIST where BUYER_SELLER_PER_LAKH_N >3000 AND TO_CHAR(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'"+" and symbol='"+symbol+"' )"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                results = cursor.fetchall()
+                for row in results:
+                    if row[0]=='S':
+                        return(3)
+                    else:
+                        return(2)
+                return(1)    
+        #symbol_1 = st.text_input('Enter symbol Name')
+        dateTimeObj=str(ip_date)
+        st.write(dateTimeObj)
+        query = "SELECT * FROM (SELECT A.symbol ,max(a.cul_weight_n)  weight FROM DAILY_NSE_MOVEMENT_TRANS_HIST A , FIVEDAYS_AVG_TBL_HIST B WHERE a.BUYER_SELLER_PER_LAKH_N>0 AND a.BUYER_SELLER_PER_LAKH_N IS NOT NULL  AND  A.SYMBOL=B.SYMBOL AND  to_char(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"' AND to_char(RECORD_DATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"'  GROUP BY a.SYMBOL order by 2 DESC) WHERE ROWNUM <6"
+        #st.write(query)
+        symbol=[]
+        weight=[]
+        incra=[]
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            results = cursor.fetchall()
+        for row in results:
+            symbol.append(row[0])
+            weight.append(row[1])
+            incra.append(incranation(row[0]))
+        #tuples = [(key, value) for i, (key, value) in enumerate(zip(symbol, weight))]
+        # convert list of tuples to dictionary using dict()
+        #symbol_dic = dict(tuples)
+        #print(symbol_dic)
+        #symbol_1 = list(symbol_dic.keys())
+        #weight_1 = list(symbol_dic.values())
+        import matplotlib.pyplot as plt
+        fig = plt.figure(figsize = (20, 8))
+        #print(incra)
+        colors = ['r' if t > 2 else ( 'y' if t>1 else 'g' )for t in incra]
+        plt.margins(x=0)
+        fig, ax = plt.subplots()
+        width = 0.75
+        #plt.bar(symbol,weight,width=.8,color=colors,align='center')
+        plt.barh(symbol,weight,color=colors,align='center')
+        from datetime import datetime
+        plt.xlabel(str(datetime.strftime(ip_date, "%d-%b-%Y")))
+        #plt.xticks(range(len(symbol)), symbol, rotation='vertical',fontsize='15')
+        for index, value in enumerate(weight):
+            plt.text(value, index, str(value))
+            
+        plt.ylabel("Symbol")
+        plt.title('Top 5 weighted symbol', fontweight='bold', color = '#7FFFD4', fontsize='18')
+        st.pyplot(fig)
+    if selected_metrics_1=='Incranation History':
+              
+        #Converting string date into datetime object
+        #ip_date =st.date_input("Any trading date", format="DD/MM/YYYY", disabled=False, label_visibility="visible")
+        dateTimeObj=str(ip_date)
+        st.write(dateTimeObj)
+        # Execute a sample query
+        query = "SELECT symbol,cul_weight_n,RATIO_F_V FROM DAILY_NSE_MOVEMENT_TRANS_HIST where  BUYER_SELLER_PER_LAKH_N > 3000 and to_char(LAST_UPDATE_DT,'YYYY-MM-DD')='"+dateTimeObj+"' "
+        #st.write(query)
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            results = cursor.fetchall()
+        #Display the query results
+        st.write("Query Results:")
+        for row in results:
+            #st.write(row)
+            #st.success(row)
+            st.write(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{row}</p>', unsafe_allow_html=True)
+
+
