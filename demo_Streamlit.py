3e86d17b91752c537fce0d816380d03e2fd837a4
rasbihari Das <48406292+rdas77@users.noreply.github.com>
2024-02-17 17:45:27 +0530

    Add files via upload

---
+++ demo_Streamlit.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+df = pd.DataFrame(
+np.random.randn(50, 20),
+columns=('col %d' % i for i in range(20)))
+print(df)
+st.dataframe(df)
